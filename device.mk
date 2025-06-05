87bab0b65b4c015a7911ea4121065b371bf5318b
Mdlove59 <benjii280@gmail.com>
2025-06-05 11:08:52 +0200

    Update device.mk

--- device.mk
+++ device.mk
@@ -18,15 +18,14 @@ AB_OTA_POSTINSTALL_CONFIG += \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
+# Boot Control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-service
+    android.hardware.boot@1.2-impl \
+    android.hardware.boot@1.2-impl.recovery \
+    android.hardware.boot@1.2-service
 
-# Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-service \
-    android.hardware.boot@1.1-service  \
-    android.hardware.boot@1.2-service \
+    android.hardware.boot@1.2-mtkimpl.recovery \
     libmtk_bsg.recovery
 
 PRODUCT_PACKAGES+= \
